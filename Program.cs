7ca7cd6bbfe2327b39e5b60f623e9f89fb090b34
JorgenMJobloop <jorgen@jobloop.no>
2025-12-01 10:13:56 +0100

    added examples

--- Program.cs
+++ Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // variables & datatypes
+        // explicit type casting
+        string helloWorld = "Hello, World!";
+        Console.WriteLine(helloWorld);
+        // implicit type casting (hinted types, compiler recognizes the type at runtime)
+        // var myNumber = 25; // integer
+        int myNumber = 25; // integer, explicit cast
+        Console.WriteLine(myNumber * 2.5);
+        double myDecimalNumber = 25.5; // double
+        Console.WriteLine(myNumber + myDecimalNumber);
+        bool shouldBeTrue = true;
+        Console.WriteLine(shouldBeTrue);
+        char a = 'a';
+        char b = 'b';
+        Console.WriteLine(a + b);
+        /*
+        Special data types
+        */
+        byte myByte = 0x1; // 0 bytes assigned
+        Console.WriteLine(myByte);
     }
 }
